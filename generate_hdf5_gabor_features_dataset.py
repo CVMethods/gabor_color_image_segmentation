fd0ce295c30b024b096aed4fb5471b5f0886c2f9
Eric Bazan <ericbzn@gmail.com>
2020-07-02 09:56:09 +0200

    Remove complex image from the dataset & add filterbank data as metadata
    This operation allows to keep the hdf5 data set small

--- generate_hdf5_gabor_features_dataset.py
+++ generate_hdf5_gabor_features_dataset.py
@@ -21,11 +21,11 @@ class ImageIndexer(object):
         self.num_of_images = num_of_images
         self.fixed_image_shape = fixed_image_shape
         self.fixed_features_shape = fixed_features_shape
-        self.image_vector_db = None
+        # self.image_vector_db = None
         self.feature_vector_db = None
         self.idxs = {"index": 0}
 
-        self.image_vector_buffer = []
+        # self.image_vector_buffer = []
         self.feature_vector_buffer = []
 
     def __enter__(self):
@@ -39,7 +39,7 @@ class ImageIndexer(object):
             print(len(self.feature_vector_buffer))
 
             self._write_buffer(self.feature_vector_db, self.feature_vector_buffer)
-            self._write_buffer(self.image_vector_db, self.image_vector_buffer)
+            # self._write_buffer(self.image_vector_db, self.image_vector_buffer)
 
         print("closing h5 db")
         self.db.close()
@@ -55,36 +55,36 @@ class ImageIndexer(object):
     def create_datasets(self):
         IMG_ROWS, IMG_COLS, CHANN = self.fixed_image_shape
         IMG_PXLS, IMG_FEAT = self.fixed_features_shape
-
-        self.image_vector_db = self.db.create_dataset(
-            "complex_images",
-            shape=(self.num_of_images, 3, IMG_ROWS * IMG_COLS),
-            maxshape=(self.num_of_images, None, None),
-            dtype=np.float32,
-            chunks=True
-        )
+        #
+        # self.image_vector_db = self.db.create_dataset(
+        #     "complex_images",
+        #     shape=(self.num_of_images, 3, IMG_ROWS * IMG_COLS),
+        #     maxshape=(self.num_of_images, None, None),
+        #     dtype=np.float32,
+        #     chunks=True
+        # )
 
         self.feature_vector_db = self.db.create_dataset(
             "gabor_features",
             shape=(self.num_of_images, IMG_PXLS, IMG_FEAT),
             maxshape=(self.num_of_images, None, None),
             dtype=np.float32,
-            chunks=True
+            chunks=(1, IMG_PXLS, IMG_FEAT)
         )
 
-    def add(self, image_vector, feature_vector):
-        self.image_vector_buffer.append(image_vector.reshape(self.image_vector_size))
+    def add(self, feature_vector):#image_vector,
+        # self.image_vector_buffer.append(image_vector.reshape(self.image_vector_size))
         self.feature_vector_buffer.append(feature_vector)
 
-        if None in (self.image_vector_db, self.feature_vector_db):
+        if self.feature_vector_db is None:#if None in (self.feature_vector_db):#self.image_vector_db,
             self.create_datasets()
 
         if len(self.feature_vector_buffer) >= self.buffer_size:
-            self._write_buffer(self.image_vector_db, self.image_vector_buffer)
+            # self._write_buffer(self.image_vector_db, self.image_vector_buffer)
             self._write_buffer(self.feature_vector_db, self.feature_vector_buffer)
 
             # increment index
-            self.idxs['index'] += len(self.image_vector_buffer)
+            self.idxs['index'] += len(self.feature_vector_buffer)
 
             # clean buffers
             self._clean_buffers()
@@ -96,7 +96,7 @@ class ImageIndexer(object):
         dataset[start:start + end] = buf
 
     def _clean_buffers(self):
-        self.image_vector_buffer = []
+        # self.image_vector_buffer = []
         self.feature_vector_buffer = []
 
 
@@ -134,18 +134,17 @@ if __name__ == '__main__':
 
     # Generating Gabor filterbank
     min_period = 2.
-    max_period = 35.
-    fb = 1  # 0.7  #
-    ab = 45  # 30  #
+    max_period = 25.
+    fb = 0.7  # 0.7  #
+    ab = 30  # 30  #
     c1 = 0.65
     c2 = 0.65
-    stds = 3.5
+    stds = 3.0
     print('Generating Gabor filterbank')
     gabor_filters, frequencies, angles = makeGabor_filterbank(min_period, max_period, fb, ab, c1, c2, stds)
 
     n_freq = len(frequencies)
     n_angles = len(angles)
-    color_space = 'HS'
     color_space = 'HS'
 
     r_type = 'L2'  # 'real'
@@ -156,11 +155,13 @@ if __name__ == '__main__':
 
     # Read hdf5 file and extract its information
     print('Reading Berkeley image data set')
+    t0 = time.time()
     file = h5py.File(hdf5_dir / "Berkeley_images.h5", "r+")
     image_vectors = np.array(file["/images"])
     img_shapes = np.array(file["/image_shapes"])
     img_ids = np.array(file["/image_ids"])
-    num_segments = np.array(file["/num_seg"])
+    t1 = time.time()
+    print('Reading hdf5 image data set time: %.2fs' % (t1 - t0))
 
     images = Parallel(n_jobs=num_cores)(
         delayed(np.reshape)(img, (shape[0], shape[1], shape[2])) for img, shape in zip(image_vectors, img_shapes))
@@ -176,14 +177,25 @@ if __name__ == '__main__':
     t1 = time.time()
     print('Features computing time (using Parallel joblib): %.2fs' % (t1 - t0))
 
-    with ImageIndexer(hdf5_dir / "Berkeley_GaborFeatures.h5",
+    with ImageIndexer(hdf5_dir / "Berkeley_GaborFeatures_attrs.h5",
                       fixed_image_shape=(481, 321, 3),
                       fixed_features_shape=(img_shapes[0][0]*img_shapes[0][1], n_freq*n_angles*img_shapes[0][2]),
-                      buffer_size=num_imgs,
+                      buffer_size=10,
                       num_of_images=num_imgs) as imageindexer:
 
-        for img, features in zip(twoChannel_imgs, gabor_features):
-            imageindexer.add(img, features)
+        # for img, features in zip(gabor_features):#twoChannel_imgs,
+        #     imageindexer.add(features)#img,
+        for features in gabor_features:#twoChannel_imgs,
+            imageindexer.add(features)#img,
+            imageindexer.db.attrs['num_freq'] = n_freq
+            imageindexer.db.attrs['num_angles'] = n_angles
+            imageindexer.db.attrs['min_period'] = min_period
+            imageindexer.db.attrs['max_period'] = max_period
+            imageindexer.db.attrs['frequency_bandwidth'] = fb
+            imageindexer.db.attrs['angular_bandwidth'] = ab
+            imageindexer.db.attrs['crossing_point1'] = c1
+            imageindexer.db.attrs['crossing_point2'] = c2
+            imageindexer.db.attrs['num_stds'] = stds
         # Parallel(n_jobs=num_cores)(delayed(imageindexer.add)(img, features) for img, features in zip(twoChannel_imgs, gabor_features))
 
     # # Read hdf5 file and extract its information
